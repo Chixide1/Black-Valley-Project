b75d3213832252d625a1e7cac8c4c89ae329a05c
Chixide1 <chixide12@gmail.com>
2021-04-19 23:55:52 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,9 +7,36 @@ provider "azurerm" {
 }
 
 #AWS
+resource "aws_vpc" "virtpc" {
+  cidr_block = "192.168.0.0/24"
+
+  tags = {
+    "Name" = "bv-vpc"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id = aws_vpc.virtpc.id
+  cidr_block = "192.168.0.0/25"
+  availability_zone = "eu-west-2b"
+
+  tags = {
+    "Name" = "bv-vpc"
+  }
+}
+
+resource "aws_network_interface" "netint" {
+  subnet_id = aws_subnet.subnet.id
+}
+
 resource "aws_instance" "vm-aws" {
   ami = "ami-0fbec3e0504ee1970"
   instance_type = "t2.micro"
+
+  network_interface {
+    device_index = 0
+    network_interface_id = aws_network_interface.netint.id
+  }
 
   tags = {
     "Name" = "bv-i"
@@ -54,5 +81,35 @@ resource "azurerm_network_interface" "nic" {
     name = "bv-nic"
     subnet_id = azurerm_subnet.sn.id
     private_ip_address_allocation = "dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name = "bv-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = "westeurope"
+  vm_size = "Standard_B1s"
+
+  network_interface_ids = [ azurerm_network_interface.nic.id ]
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
   }
 }
